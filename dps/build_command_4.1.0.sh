8b190d12ed099941c9465fbbe613da0d21f34fb8
montesano <paul.montesano@gmail.com>
2025-11-13 12:02:04 -0800

    dps prep, use build sh with env yml to install geedim

--- dps/build_command_4.1.0.sh
+++ dps/build_command_4.1.0.sh
@@ -4,7 +4,7 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P )
 
 # This is designed to update the base image called 'python'
 # https://github.com/MAAP-Project/maap-workspaces/blob/main/base_images/python/environment.yml
-conda env update -f ${basedir}/above_env_4.1.0.yml --solver=libmamba
+conda env update -f ${basedir}/maap_tools_env_4.1.0.yml --solver=libmamba
 
 # New name of the conda env created in above_env_4.1.0.yml
 source activate python
